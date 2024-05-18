7171a29e09e8a649e0a1524e5a9291a87d120723
bobojeager <danishhilmann98@gmail.com>
2024-05-18 13:18:19 +0800

    added patch service

--- src/main/java/dev/matinWebsite/matinwebapi/CharacterService.java
+++ src/main/java/dev/matinWebsite/matinwebapi/CharacterService.java
@@ -4,7 +4,9 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CharacterService {
@@ -19,5 +21,26 @@ public class CharacterService {
     }
 
     Characters CreateCharacter(Characters character){return  characterRepository.save(character);}
+
+    Characters PatchCharacter(String id, Map<String,Object> payload){
+        ObjectId msgId = new ObjectId(id);
+        Characters character = characterRepository.findById(msgId).orElse(null);
+        if (character == null){
+            return null;
+        }
+        for (Map.Entry<String, Object> entry: payload.entrySet()){
+            String fieldname = entry.getKey();
+            Object val = entry.getValue();
+
+            try {
+                Field field = Characters.class.getDeclaredField(fieldname);
+                field.setAccessible(true);
+                field.set(character, val);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return characterRepository.save(character);
+    }
 
 }
